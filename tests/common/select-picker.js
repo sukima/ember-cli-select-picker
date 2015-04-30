7537edfffb7007b3cd05c5ec5cb872250b7d9b93
Devin Weaver <suki@tritarget.org>
2015-04-29 21:45:38 -0400

    Add semi-comprehensive unit tests
    Not full coverage but good enough for behavior. Still need acceptance
    testing but this will do for now.
    
    Several bugs were found with these tests and a forthcoming commit will
    fix them.

---
+++ tests/common/select-picker.js
@@ -0,0 +1,148 @@
+import Ember from 'ember';
+import { test } from 'ember-qunit';
+
+const contentArray = [
+  {name: 'First item',  id: 1},
+  {name: 'Second item', id: 2},
+  {name: 'Third item',  id: 3},
+  {name: 'Another',     id: 4}
+];
+
+export default function() {
+  test('it renders', function(assert) {
+    assert.expect(2);
+
+    // creates the component instance
+    var component = this.subject();
+    assert.equal(component._state, 'preRender');
+
+    // renders the component to the page
+    this.render();
+    assert.equal(component._state, 'inDOM');
+  });
+
+  test('updates contentList when selection changes', function(assert) {
+    assert.expect(3);
+
+    var component = this.subject({
+      multiple: true,
+      content: contentArray,
+      selection: Ember.A(),
+      optionValuePath: 'content.id',
+      optionLabelPath: 'content.name'
+    });
+
+    Ember.run(function() {
+      component.send('selectAllNone', 'unselectedContentList');
+    });
+
+    assert.equal(
+      component.get('selection.length'), contentArray.length,
+      'all items should be selected'
+    );
+
+    Ember.run(function() {
+      component.send('selectAllNone', 'selectedContentList');
+    });
+
+    assert.equal(
+      component.get('selection.length'), 0,
+      'no items should be selected'
+    );
+
+    Ember.run(function() {
+      component.send('selectItem', component.get('contentList.firstObject'));
+    });
+
+    assert.equal(
+      component.get('selection.length'), 1,
+      'no items should be selected'
+    );
+  });
+
+  test('Normal Search', function(assert) {
+    assert.expect(5);
+
+    var component = this.subject({
+      multiple: true,
+      liveSearch: true,
+      content: contentArray,
+      selection: Ember.A(),
+      optionValuePath: 'content.id',
+      optionLabelPath: 'content.name'
+    });
+
+    assert.equal(
+      component.get('contentList.length'), contentArray.length,
+      'contentList should not be filtered initially'
+    );
+
+    Ember.run(function() {
+      component.set('searchFilter', 'F');
+    });
+
+    // Only one Items has 'F' litter in it (First Item), So, Filtering should
+    // result in one item being listed.
+    assert.equal(
+      component.get('contentList.length'), 1,
+      'contentList should be filtered to one result for "F"'
+    );
+
+    Ember.run(function() {
+      component.set('searchFilter', 'S');
+    });
+
+    // The result should be 2 (First, Second)
+    assert.equal(
+      component.get('contentList.length'), 2,
+      'contentList should be filtered to two results for "S"'
+    );
+
+    Ember.run(function() {
+      component.send('clearFilter');
+    });
+
+    assert.equal(
+      component.get('contentList.length'), contentArray.length,
+      'contentList should not be filtered when searchFilter is cleared'
+    );
+
+    Ember.run(function() {
+      component.set('searchFilter', 'itm');
+    });
+
+    // When search is set to normal (not advanced), searching for 'itm' should
+    // give 0 results
+    assert.equal(
+      component.get('contentList.length'), 0,
+      'contentList should be filtered to no results for "itm"'
+    );
+  });
+
+  test('Advanced Search', function(assert) {
+    assert.expect(2);
+
+    var component = this.subject({
+      multiple: true,
+      liveSearch: 'advanced',
+      content: contentArray,
+      selection: Ember.A(),
+      optionValuePath: 'content.id',
+      optionLabelPath: 'content.name'
+    });
+
+    assert.equal(
+      component.get('contentList.length'), contentArray.length,
+      'contentList should not be filtered initially'
+    );
+
+    Ember.run(function() {
+      component.set('searchFilter', 'itm');
+    });
+
+    assert.equal(
+      component.get('contentList.length'), 3,
+      'contentList should be filtered to three results for "itm"'
+    );
+  });
+}
--- tests/unit/components/list-picker-test.js
+++ tests/unit/components/list-picker-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent } from 'ember-qunit';
 import commonPickerTests from '../../common/select-picker';
 
-moduleForComponent('select-picker', {
+moduleForComponent('list-picker', {
   // specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
 });
