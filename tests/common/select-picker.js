e2002654ba91984571f0cb90768c1757f6e9ea18
Devin Weaver <suki@tritarget.org>
2015-04-30 11:02:40 -0400

    Remove non-sense tests
    Since we are doing integration tests the 'it renders' specs are silly.
    They are also broken in Ember >= 1.11

--- tests/common/select-picker.js
+++ tests/common/select-picker.js
@@ -9,18 +9,6 @@ const contentArray = [
 ];
 
 export default function() {
-  test('it renders', function(assert) {
-    assert.expect(2);
-
-    // creates the component instance
-    var component = this.subject();
-    assert.equal(component._state, 'preRender');
-
-    // renders the component to the page
-    this.render();
-    assert.equal(component._state, 'inDOM');
-  });
-
   test('updates contentList when selection changes', function(assert) {
     assert.expect(3);
 
