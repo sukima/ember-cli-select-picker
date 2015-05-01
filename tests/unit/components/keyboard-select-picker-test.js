cf25c69cedc169c47a04ab34c07d8627a4328ab9
Devin Weaver <suki@tritarget.org>
2015-05-01 14:05:54 -0400

    Add unit tests for KeyboardSelectPickerComponent
    We can't add integtration tests yet because the ember-keyboard-shortcut
    addon was not writen to support EXTEND_PROTOTYPES:false
    
    Will add integration tests after that addon is updated to
    Ember-CLI >= 0.2.3

---
+++ tests/unit/components/keyboard-select-picker-test.js
@@ -0,0 +1,9 @@
+import { moduleForComponent } from 'ember-qunit';
+import commonPickerTests from '../../common/select-picker';
+
+moduleForComponent('keyboard-select-picker', {
+  // specify the other units that are required for this test
+  // needs: ['component:foo', 'helper:bar']
+});
+
+commonPickerTests();
